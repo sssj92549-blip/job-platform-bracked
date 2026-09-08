6841093c488945d685023cc93bf17651fea42185
sssj92549-blip <sssj92549@gmail.com>
2026-09-09 07:38:54 +0800

    初始提交：项目代码

---
+++ src/main/java/cn/itcast/demo/jobplatform/JobPlatformApplication.java
@@ -0,0 +1,13 @@
+package cn.itcast.demo.jobplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/itcast/demo/jobplatform/JobPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.itcast.demo.jobplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
